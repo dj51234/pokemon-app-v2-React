98ab5b554014a2882cef5ba5a4fdcb978e8b6dbf
dj51234 <dj51234@yahoo.com>
2024-07-24 00:46:53 -0400

    updated box shadow transition for card component

--- src/components/PackOpening.js
+++ src/components/PackOpening.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../styles/PackOpening.css';
 import defaultImage from '../assets/default-image.png';
 import NormalCard from './NormalCard';
@@ -13,6 +13,7 @@ const PackOpening = ({ show, randomCards, onBack, onNext, addRevealedCards }) =>
   const [hideStack, setHideStack] = useState(false);
   const [highlightBackButton, setHighlightBackButton] = useState(false);
   const [hideNextButton, setHideNextButton] = useState(false);
+  const topCardRef = useRef(null);
 
   useEffect(() => {
     if (randomCards.length > 0) {
@@ -60,14 +61,41 @@ const PackOpening = ({ show, randomCards, onBack, onNext, addRevealedCards }) =>
       setAnimating(true);
       setMovingCard(index);
 
+      if (index === 0 && newLeftStack[1]) {
+        const siblingCard = newLeftStack[1];
+        if (isRare(siblingCard.rarity)) {
+          siblingCard.class = 'rare-card';
+        }
+      }
+
       setTimeout(() => {
         const newCard = newLeftStack.splice(index, 1)[0];
         newLeftStack.push(newCard);
+        newLeftStack.forEach((card, idx) => {
+          card.zIndex = newLeftStack.length - idx;
+        });
+
+        if (index === 0 && newCard.class === 'rare-card') {
+          newCard.class = '';
+        }
+
         setLeftStack(newLeftStack);
         setMovingCard(null);
         setAnimating(false);
       }, 700);
     }
+  };
+
+  const isRare = (rarity) => {
+    const rareRarities = [
+      'special illustration rare', 'ace spec rare', 'amazing rare', 'hyper rare', 'double rare', 
+      'radiant rare', 'illustration rare', 'rare ace', 'rare holo', 'rare break', 'rare holo ex',
+      'rare holo gx', 'rare holo lv.x', 'rare holo star', 'rare v', 'rare holo vmax',
+      'rare rare holo vstar', 'rare prime', 'rare prism star', 'rare rainbow', 'rare secret',
+      'rare shining', 'rare holo shiny', 'rare shiny gx', 'rare ultra', 'shiny rare', 
+      'shiny ultra rare', 'trainer gallery rare holo', 'ultra rare'
+    ];
+    return rareRarities.includes(rarity);
   };
 
   const handleBackClick = () => {
@@ -104,7 +132,8 @@ const PackOpening = ({ show, randomCards, onBack, onNext, addRevealedCards }) =>
           {leftStack.map((card, index) => (
             <div
               key={index}
-              className={`card ${movingCard === index ? 'moving-to-back' : ''}`}
+              ref={index === 0 ? topCardRef : null}
+              className={`card ${movingCard === index ? 'moving-to-back' : ''} ${card.class || ''}`}
               style={{ zIndex: leftStack.length - index }}
             >
               <NormalCard
