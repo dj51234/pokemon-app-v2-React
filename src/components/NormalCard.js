a0d21327d6f854ea930bc8367d3c8b8bfea3ceff
dj51234 <dj51234@yahoo.com>
2024-08-26 02:28:48 -0400

    expandedCardView complete

--- src/components/NormalCard.js
+++ src/components/NormalCard.js
@@ -76,13 +76,13 @@ const NormalCard = React.forwardRef(({
     if (applyBoxShadow) {
       switch (rarity) {
         case 'ace spec rare':
-          setBoxShadow('0 0 3px -1px #F700C1, 0 0 5px 1px #F700C1, 0 0 22px 2px #F700C1, 0px 10px 20px -5px black, 0 0 40px -30px #F700C1, 0 0 50px -20px #F700C1');
+          setBoxShadow('0 0 1px -1px #F700C1, 0 0 3px 1px #F700C1, 0 0 12px 2px #F700C1, 0px 10px 20px -5px black, 0 0 20px -30px #F700C1, 0 0 50px -20px #F700C1');
           break;
         case 'double rare':
           setBoxShadow('0 0 3px -1px white, 0 0 5px 1px white, 0 0 22px 2px white, 0px 10px 20px -5px black, 0 0 40px -30px white, 0 0 50px -20px white');
           break;
         case 'hyper rare':
-          setBoxShadow('0 0 3px -1px #FFD913, 0 0 5px 1px #FFD913, 0 0 22px 2px #FFD913, 0px 10px 20px -5px black, 0 0 40px -30px #FFD913, 0 0 50px -20px #FFD913');
+          setBoxShadow('0 0 1px -1px #FFD913, 0 0 3px 1px #FFD913, 0 0 12px 2px #FFD913, 0px 10px 20px -5px black, 0 0 20px -30px #FFD913, 0 0 50px -20px #FFD913');
           break;
         case 'rare holo':
           setBoxShadow('0 0 3px -1px white, 0 0 5px 1px white, 0 0 22px 2px white, 0px 10px 20px -5px black, 0 0 40px -30px white, 0 0 50px -20px white');
--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -58,11 +58,11 @@ const rarityColors = {
 const getBoxShadowForRarity = (rarity) => {
   switch (rarity) {
     case 'ace spec rare':
-      return '0 0 3px -1px #F700C1, 0 0 5px 1px #F700C1, 0 0 22px 2px #F700C1, 0px 10px 20px -5px black, 0 0 40px -30px #F700C1, 0 0 50px -20px #F700C1';
+      return '0 0 1px -1px #F700C1, 0 0 3px 1px #F700C1, 0 0 12px 2px #F700C1, 0px 10px 20px -5px black, 0 0 20px -30px #F700C1, 0 0 50px -20px #F700C1';
     case 'double rare':
       return '0 0 3px -1px white, 0 0 5px 1px white, 0 0 22px 2px white, 0px 10px 20px -5px black, 0 0 40px -30px white, 0 0 50px -20px white';
     case 'hyper rare':
-      return '0 0 3px -1px #FFD913, 0 0 5px 1px #FFD913, 0 0 22px 2px #FFD913, 0px 10px 20px -5px black, 0 0 40px -30px #FFD913, 0 0 50px -20px #FFD913';
+      return '0 0 1px -1px #FFD913, 0 0 3px 1px #FFD913, 0 0 12px 2px #FFD913, 0px 10px 20px -5px black, 0 0 20px -30px #FFD913, 0 0 50px -20px #FFD913';
     case 'rare holo':
       return '0 0 3px -1px #FFFFFF, 0 0 5px 1px #FFFFFF, 0 0 22px 2px #FFFFFF, 0px 10px 20px -5px black, 0 0 40px -30px #FFFFFF, 0 0 50px -20px #FFFFFF';
     case 'illustration rare':
